983e4a46fa474edf9ae975b6c427beac3cddc696
StefansArya <stefansarya1@gmail.com>
2021-07-18 13:42:14 +0700

    Fix error on views hot reload that shows `index.sf` was missing
    Signed-off-by: StefansArya <stefansarya1@gmail.com>

--- src/helper.js
+++ src/helper.js
@@ -99,7 +99,7 @@ module.exports = {
 
 							insideOfView = true;
 							code += `sf.Views._$edit(${view}, [`;
-							if(routes['index.sf'] === void 0)
+							if(routes[key]['index.sf'] === void 0)
 								code += '{path:"/", html:"\'index.sf\' was not found"},';
 
 							diveRoute(routes[key]);
